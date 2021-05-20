fa80817fd4f1fa2204c279d23a232780023547ca
Mike DelGaudio <mdelgaud@stevens.edu>
2021-05-19 21:44:48 -0400

    first attempt to graphql

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const { graphqlHTTP } = require("express-graphql");
+const { GraphQLSchema, GraphQLObjectType, GraphQLString } = require("graphql");
+const app = express();
+
+const schema = new GraphQLSchema({
+  query: new GraphQLObjectType({
+    name: "Helloworld",
+    fields: () => ({
+      message: { type: GraphQLString, resolve: () => "Hello world" },
+    }),
+  }),
+});
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema: schema,
+    graphiql: true,
+  })
+);
+
+app.listen(5000, () => console.log("server on"));
